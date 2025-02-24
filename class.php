0b2b012e8fac6db69e7b93affb0dedf52b2af997
Pentrelli Giuseppe <peppepentrelli95@gmail.com>
2025-02-24 17:57:23 +0100

    Completato Selfwork PHP_OPP_05

---
+++ class.php
@@ -0,0 +1,71 @@
+<?php 
+
+// Classe Genitore
+
+abstract class Category { 
+
+public abstract function getMyCategory();
+
+}
+
+//!  Classi Figlie
+
+class Attualita extends Category{ 
+ 
+    public function getMyCategory() { 
+    
+        return "Categoria: Attualità\n";
+   
+    }
+    
+}
+
+class Sport extends Category{ 
+    
+    public function getMyCategory() { 
+    
+        return "Categoria: Sport\n";
+    
+    }
+
+}
+
+class Gossip extends Category{ 
+
+    public function getMyCategory() { 
+    
+        return "Categoria: Gossip\n";
+    
+    }
+    
+}
+
+class Storia extends Category{ 
+    
+    public function getMyCategory() { 
+    
+        return "Categoria: Storia\n";
+    
+    }
+
+}
+
+// Attualita
+$attualita = new Attualita();
+$attualita->getMyCategory();
+echo $attualita->getMyCategory();
+
+// Sport
+$sport = new Sport();
+$sport->getMyCategory();
+echo $sport->getMyCategory();
+
+// Gossip
+$gossip = new Gossip();
+$gossip->getMyCategory();
+echo $gossip->getMyCategory();
+
+// Storia
+$storia = new Storia();
+$storia->getMyCategory();
+echo $storia->getMyCategory();
